e5b9312ce25df9ba8d5bbd70b32cb38443b50e82
shina <zodiac_oracle@docomo.ne.jp>
2022-12-04 23:31:23 +0900

    Add outline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+const INCREMENT: char = '+';
+const DECREMENT: char = '-';
+const RIGHT: char = '>';
+const LEFT: char = '<';
+const LOOP_START: char = '[';
+const LOOP_END: char = ']';
+const OUTPUT: char = '.';
+const INPUT: char = ',';
+
+const MEMORY_SIZE: usize = 1024;
+
 fn main() {
-    println!("Hello, world!");
+    let mut memory: [u8; MEMORY_SIZE] = [0; MEMORY_SIZE];
+    let mut ptr: usize = 0;
+    let mut code_ptr: usize = 0;
 }
