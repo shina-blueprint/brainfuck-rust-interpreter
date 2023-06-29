b52e80112a48464f40580bc1b3c3b85bf51da9ba
shina <shina.blueprint@gmail.com>
2023-06-30 00:04:07 +0900

    Add loop instruction

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ fn main() {
     let mut ptr: usize = 0;
     let mut code_ptr: usize = 0;
 
+    let mut loops: Vec<usize> = vec![0; 0];
+
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("Error: A Brainfuck code is not passed as a command-line argument.");
@@ -38,9 +40,34 @@ fn main() {
 
             LEFT => ptr = if ptr <= 0 { MEMORY_SIZE - 1 } else { ptr - 1 },
 
-            LOOP_START => (),
+            LOOP_START => {
+                loops.push(code_ptr);
+                if memory[ptr] == 0 {
+                    let mut depth = 1;
+                    while depth > 0 {
+                        code_ptr += 1;
+                        if code_ptr >= code_len {
+                            eprintln!("Error: Loop end order, {}, is not found.", LOOP_END);
+                            process::exit(1);
+                        }
+                        if code.chars().nth(code_ptr).unwrap() == LOOP_START {
+                            depth += 1;
+                        }
+                        if code.chars().nth(code_ptr).unwrap() == LOOP_END {
+                            depth -= 1;
+                        }
+                    }
+                    loops.pop();
+                }
+            }
 
-            LOOP_END => (),
+            LOOP_END => match loops.pop() {
+                Some(loops_ptr) => code_ptr = loops_ptr - 1,
+                None => {
+                    eprintln!("Error: Loop start order, {}, is not found.", LOOP_START);
+                    process::exit(1);
+                }
+            },
 
             OUTPUT => print!("{}", memory[ptr] as char),
 
