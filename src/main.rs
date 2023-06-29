a5cdca863329df6e494ddc9f7279d657434408af
shina <shina.blueprint@gmail.com>
2023-06-29 23:53:34 +0900

    Add outline

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+const INCREMENT: char = '+';
+const DECREMENT: char = '-';
+const RIGHT: char = '>';
+const LEFT: char = '<';
+const LOOP_START: char = '[';
+const LOOP_END: char = ']';
+const OUTPUT: char = '.';
+const INPUT: char = ',';
+
+const MEMORY_SIZE: usize = 1024;
+
+fn main() {
+    let mut memory: [u8; MEMORY_SIZE] = [0; MEMORY_SIZE];
+    let mut ptr: usize = 0;
+    let mut code_ptr: usize = 0;
+}
